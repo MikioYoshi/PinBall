c4de743b1d8adaa9e8badb169c1ae3792d19cf48
MikioYoshi <71675915+MikioYoshi@users.noreply.github.com>
2020-10-04 02:27:50 +0900

    スコアの表示
    ボールがターゲットに当たるたびにスコアを加算し表示されるようにした。

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -9,11 +9,17 @@ public class BallController : MonoBehaviour
 
     private GameObject gameoverText;
 
+    private GameObject scoreText;
+
+    private int score = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.gameoverText = GameObject.Find("GameOverText");
+
+        this.scoreText = GameObject.Find("ScoreText");
     }
 
     // Update is called once per frame
@@ -24,4 +30,22 @@ public class BallController : MonoBehaviour
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
     }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.score += 30;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.score += 50;
+        }
+        this.scoreText.GetComponent<Text>().text = this.score.ToString();
+    }
+        
 }
